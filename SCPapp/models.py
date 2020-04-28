af29a774385fe2443c90b5610f74538329ff99d2
Swati N Murthy <swati.snm20@gmail.com>
2020-04-28 20:21:11 +0530

    Adding fields in model
    Sample only, should be changed later.
    Signed-off-by: Swati N Murthy <swati.snm20@gmail.com>

--- SCPapp/models.py
+++ SCPapp/models.py
@@ -4,7 +4,10 @@ from django.db.models import CharField, Model
 
 class File(models.Model):
     file = models.FileField(blank=False, null=False)
-    subject=models.CharField(max_length=101)
-    year=models.CharField(max_length=10)
+    subject = models.CharField(max_length=101)
+    year = models.CharField(max_length=10)
+    resourceType = models.CharField(max_length=20, blank=True, null=True)
+    semester = models.IntegerField(blank=True, null=True)
+
     def __str__(self):
         return self.file.name
