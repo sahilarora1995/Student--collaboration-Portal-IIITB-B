ea1366cbaeac0cc4189ceea6e788155dd1d73e9d
Sahil Arora <Sahil Arora>
2020-04-30 18:15:52 +0530

    patch added

---
+++ SCPapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.core.files.base import File
+from django.db.models import CharField, Model
+
+class File(models.Model):
+    file = models.FileField(blank=False, null=False)
+    subject = models.CharField(max_length=101)
+    year = models.IntegerField()
+    resourceType = models.CharField(max_length=20, blank=True, null=True)
+    semester = models.IntegerField()
+    numberofUpvotes = models.IntegerField(default=0)
+    numberofDownvotes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.file.name
---
+++ SCPapp/views.py
@@ -0,0 +1,51 @@
+from rest_framework.parsers import FileUploadParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from .models import File
+from .serializers import serializers
+
+from .serializers import FileSerializer
+
+class getData(APIView):
+    def get(self, request, *args, **kwargs):
+        allFiles = File.objects.all()
+        for key in request.data.keys():
+            value = request.data.get(key)
+            if (key == 'resourceType'):
+                allFiles = allFiles.filter(resourceType=value)
+            elif (key == 'semester'):
+                allFiles = allFiles.filter(semester=value)
+            elif (key == 'subject'):
+                allFiles = allFiles.filter(subject=value)
+            elif (key == 'year'):
+                allFiles = allFiles.filter(year=value)
+            elif (key == 'id'):
+                allFiles = allFiles.filter(id=value)
+        
+        serializer = FileSerializer(allFiles, many=True)
+        return Response(serializer.data)
+
+class patchData(APIView):
+    def get_object(self, id):
+        try:
+            return File.objects.get(id=id)
+        except File.DoesNotExist:
+            raise Http404
+
+    def patch(self, request, id):
+        File = self.get_object(id)
+        serializer = FileSerializer(File, data=request.data,
+                                         partial=True)  # set partial=True to update a data partially
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class postData(APIView):
+    def post(self, request, *args, **kwargs):        
+        file_serializer = FileSerializer(data=request.data)
+        if file_serializer.is_valid():
+            file_serializer.save()
+            return Response(file_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
